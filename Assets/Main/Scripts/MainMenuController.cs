fa3a88e67a362cc4c6c357f287c934198439d470
Ahmet Yüceer <ahmetyuceer008@gmail.com>
2024-05-14 13:14:37 +0300

    +

--- Assets/Main/Scripts/MainMenuController.cs
+++ Assets/Main/Scripts/MainMenuController.cs
@@ -1,40 +1,82 @@
+using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 
 public class MainMenuController : MonoBehaviour
 {
-    private UIDocument _uýDocument;
+    private UIDocument _uýDocumentMainMenu;
 
-    private Button _startButton, _settingsButton, _exitButton;
+    private VisualElement _menuElement;
+    private VisualElement _settingsElement;
+
+    private Button _startButton, _settingsButton, _exitButton, _backButton;
+    private Slider _musicSlider, _sfxSlider;
 
     private void Awake()
     {
-        _uýDocument = GetComponent<UIDocument>();
+        _uýDocumentMainMenu = GetComponent<UIDocument>();
+        _menuElement = _uýDocumentMainMenu.rootVisualElement.Q<VisualElement>("MainMenu");
+        _settingsElement = _uýDocumentMainMenu.rootVisualElement.Q<VisualElement>("SettingsMenu");
 
-        _startButton = _uýDocument.rootVisualElement.Q<Button>("StartButton");
-        _settingsButton = _uýDocument.rootVisualElement.Q<Button>("SettingsButton");
-        _exitButton = _uýDocument.rootVisualElement.Q<Button>("ExitButton");
+        //Main Menu
+        _startButton = _menuElement.Q<Button>("StartButton");
+        _settingsButton = _menuElement.Q<Button>("SettingsButton");
+        _exitButton = _menuElement.Q<Button>("ExitButton");
 
-        _startButton.clicked += StartGame;
-        _settingsButton.clicked += SettingsButton;
-        _exitButton.clicked += ExitButton;
+        _startButton.clicked += OnStartButton;
+        _settingsButton.clicked += OnSettingsButton;
+        _exitButton.clicked += OnExitButton;
+
+        //Settings Menu
+        _musicSlider = _settingsElement.Q<Slider>("MusicSlider");
+        _sfxSlider = _settingsElement.Q<Slider>("SfxSlider");
+        _backButton = _settingsElement.Q<Button>("BackButton");
+
+        _musicSlider.lowValue = 0f;
+        _musicSlider.highValue = 1f;
+        _musicSlider.value = SoundManager.Instance.GetMusicAudioSourceSoundVolume();
+
+        _sfxSlider.lowValue = 0f;
+        _sfxSlider.highValue= 1f;
+        _sfxSlider.value = SoundManager.Instance.GetSfxAudioSourceSoundVolume();
+
+        _musicSlider.RegisterValueChangedCallback(OnMusicSliderValueChanged);
+        _sfxSlider.RegisterValueChangedCallback(OnSfxSliderValueChanged);
+        _backButton.clicked += OnBackButton;
     }
 
-    private void StartGame()
+    void OnMusicSliderValueChanged(ChangeEvent<float> evt)
     {
-        Debug.Log("StartGame");
-    }
-
-    private void SettingsButton()
+        float value = evt.newValue;
+        Debug.Log("Slider deðeri: " + value);
+        SoundManager.Instance.SetMusicAudioSourceSoundVolume(value);
+    }  
+    void OnSfxSliderValueChanged(ChangeEvent<float> evt)
     {
-        Debug.Log("SettingsButton");
-
+        float value = evt.newValue;
+        SoundManager.Instance.SetSfxAudioSourceSoundVolume(value);
     }
 
-    private void ExitButton()
+    private void OnStartButton()
     {
-        Debug.Log("ExitButton");
-
+        SceneManager.LoadScene(1);
     }
 
+    private void OnSettingsButton()
+    {
+        _menuElement.style.display = DisplayStyle.None;
+        _settingsElement.style.display = DisplayStyle.Flex;
+    }
+
+    private void OnExitButton()
+    {
+        Application.Quit();
+    }
+
+    private void OnBackButton() 
+    {
+        _menuElement.style.display = DisplayStyle.Flex;
+        _settingsElement.style.display = DisplayStyle.None;
+    }
 }
---
+++ Assets/Main/Scripts/SoundManager.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance;
+
+    [Header("Audio Source")]
+    [SerializeField] private AudioSource _musicPlayer;
+    [SerializeField] private AudioSource _sfxPlayer;
+
+    [Header("Audio Clip")]
+    [SerializeField] private AudioClip _mainMenuClip;
+    [SerializeField] private AudioClip _gameLoopClip;
+    [SerializeField] private AudioClip _sfxClip;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        PlayMenuMusic();
+    }
+
+    public void SetMusicAudioSourceSoundVolume(float value)
+    {
+        _musicPlayer.volume = value;
+    }
+    public float GetMusicAudioSourceSoundVolume()
+    {
+        return _musicPlayer.volume;
+    }
+    
+    public void SetSfxAudioSourceSoundVolume(float value)
+    {
+        _sfxPlayer.volume = value;
+    }
+    public float GetSfxAudioSourceSoundVolume()
+    {
+        return _sfxPlayer.volume;
+    }
+    
+    public void PlayMenuMusic()
+    {
+        if (_musicPlayer != null)
+        {
+            _musicPlayer.clip = _mainMenuClip;
+            _musicPlayer.loop = true;
+            _musicPlayer.Play();
+        }
+    }
+    public void PlayGameLoopMusic()
+    {
+        if (_musicPlayer != null)
+        {
+            _musicPlayer.clip = _gameLoopClip;
+            _musicPlayer.loop = true;
+            _musicPlayer.Play();
+        }
+    }
+
+    public void PlaySfx()
+    {
+        if (_sfxPlayer != null && !_sfxPlayer.isPlaying)
+        {
+            _sfxPlayer.PlayOneShot(_sfxClip);
+        }
+    }
+}
